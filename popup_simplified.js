9f4b0282db0d6178a72d6b02f09e945bcdd3fa3b
michael <michaelgdimmitt@gmail.com>
2023-10-11 12:27:33 -0400

    add a quick way to tell if request succeeded or failed
    for network request refactoring
    
    This commit has been edited 1 times

--- popup_simplified.js
+++ popup_simplified.js
@@ -1,22 +1,31 @@
 console.log('hello')
+const headers = {
+  "accept": "*/*",
+  "accept-language": "en-US",
+  "apollographql-client-name": "nextjs-web",
+  "cache-control": "no-cache",
+  "content-type": "application/json",
+  "pragma": "no-cache",
+  "sec-ch-ua": "\"Google Chrome\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\"",
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": "\"macOS\"",
+  "sec-fetch-dest": "empty",
+  "sec-fetch-mode": "cors",
+  "sec-fetch-site": "same-origin",
+  "x-meetup-view-id": "ce9d9df1-ca78-40a6-9989-5c332f64cabf"
+}
+const cors = {
+  "referrer": "https://www.meetup.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+}
 const rsvpToMeetup = (decision=true, eventId="296377189", eventUrl="jax-code-and-coffee") => {
+  console.log('reached')
   const action = decision ? "YES" : "NO";
   fetch("https://www.meetup.com/gql2", {
-    "headers": {
-      "accept": "*/*",
-      "accept-language": "en-US",
-      "apollographql-client-name": "nextjs-web",
-      "cache-control": "no-cache",
-      "content-type": "application/json",
-      "pragma": "no-cache",
-      "sec-ch-ua": "\"Google Chrome\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"macOS\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-meetup-view-id": "ce9d9df1-ca78-40a6-9989-5c332f64cabf"
-    },
+    headers,
     "referrer": `https://www.meetup.com/${eventUrl}/events/${eventId}/`,
     "referrerPolicy": "strict-origin-when-cross-origin",
     "body": JSON.stringify({
@@ -40,9 +49,12 @@ const rsvpToMeetup = (decision=true, eventId="296377189", eventUrl="jax-code-and
     "method": "POST",
     "mode": "cors",
     "credentials": "include"
-  });
+  })
+  .then(res => console.log("Request 3 succeeded"))
+  .catch(res => console.log("Request 3 failed"));
+  console.log('fin')
 }
-// rsvpToMeetup(false);
+
 
 const getMeetups = (endCursor = '', count = 0) => {
   let variables;
@@ -58,21 +70,7 @@ const getMeetups = (endCursor = '', count = 0) => {
   }
   
   fetch("https://www.meetup.com/gql2", {
-    "headers": {
-      "accept": "*/*",
-      "accept-language": "en-US",
-      "apollographql-client-name": "nextjs-web",
-      "cache-control": "no-cache",
-      "content-type": "application/json",
-      "pragma": "no-cache",
-      "sec-ch-ua": "\"Google Chrome\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"macOS\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-meetup-view-id": "05a33758-5b4c-465b-bfaf-85592e388bd1"
-    },
+    headers,
     "referrer": "https://www.meetup.com/jax-code-and-coffee/events/",
     "referrerPolicy": "strict-origin-when-cross-origin",
     "body": JSON.stringify({
@@ -94,35 +92,24 @@ const getMeetups = (endCursor = '', count = 0) => {
     const eventIds = res.data.groupByUrlname.events.edges.map(event => event.node.id)
     const endCursor = res.data.groupByUrlname.events.pageInfo.endCursor
     const len = eventIds.length;
-    console.log({
+    console.log('Request 2 succeeded', {
       eventIds,
       len,
       endCursor,
       events: res.data.groupByUrlname.events
     })
-    if(len === 10 && count < 10) {
-      getMeetups(endCursor, count+1)
-    }
+    // if(len === 10 && count < 10) {
+    //   getMeetups(endCursor, count+1)
+    // }
   })
+  .catch(res => console.log("Request 2 failed"));
   // how many events do you want to rsvp out to?
   // (default 10)
 }
-// getMeetups();
+
 const getEvent = () => {
   fetch("https://api.meetup.com/gql", {
-    "headers": {
-      "accept": "application/json",
-      "accept-language": "en-US,en;q=0.9,ja;q=0.8",
-      "content-type": "application/json",
-      "sec-ch-ua": "\"Google Chrome\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\"",
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"macOS\"",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-site"
-    },
-    "referrer": "https://www.meetup.com/",
-    "referrerPolicy": "strict-origin-when-cross-origin",
+    headers,
     "body": JSON.stringify({
       query: `query($eventId: ID) {
         event(id: $eventId) {
@@ -135,11 +122,16 @@ const getEvent = () => {
         eventId: 296153162
       }
     }),
+    "referrer": "https://www.meetup.com/",
+    "referrerPolicy": "strict-origin-when-cross-origin",
     "method": "POST",
     "mode": "cors",
     "credentials": "include"
   })
   .then(res => res.json())
-  .then(res => console.log(res.data))
+  .then(res => console.log('Request 1 succeeded', res.data))
+  .catch(res => console.log("Request 1 failed"));
 }
-// getEvent();
+rsvpToMeetup(false);
+getMeetups();
+getEvent();
