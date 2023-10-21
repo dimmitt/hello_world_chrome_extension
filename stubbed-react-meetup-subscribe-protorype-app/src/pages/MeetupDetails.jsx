b6ef4404dfa135634de18ac2f1209902a6138f71
michael <michaelgdimmitt@gmail.com>
2023-10-21 15:52:37 -0400

    cleanup meetup-details page

--- stubbed-react-meetup-subscribe-protorype-app/src/pages/MeetupDetails.jsx
+++ stubbed-react-meetup-subscribe-protorype-app/src/pages/MeetupDetails.jsx
@@ -16,25 +16,25 @@ const MeetupDetails = () => {
       { created}
       <br/>description: { description}
       <br/>{ duration}
-      <br/>{ group.created}
+      {/* <br/>{ group.created}
       <br/>{ group.name}
       <br/>{ group.id}
       <br/>{ group.join_mode}
-      <br/>{ group.lat}
+      <br/>{ group.lat} */}
       <br/>id: { id}
       <br/>{ link}
-      {/* <br/>{ local_date}
-      <br/>{ local_time} */}
+      <br/>{ local_date}
+      <br/>{ local_time}
       <br/>{ name}
       <br/>{ status}
       <br/>{ JSON.stringify(time)}
       <br/>{ updated}
       <br/>{ utc_offset}
-      <br/>{ venue.id}
+      {/* <br/>{ venue.id}
       <br/>{ venue.name}
       <br/>{ venue.lat}
       <br/>{ venue.lon}
-      <br/>{ venue.repinned}
+      <br/>{ venue.repinned} */}
       <br/>{ visibility}
       <br/>{ waitlist_count}
       <br/>{ yes_rsvp_count}
