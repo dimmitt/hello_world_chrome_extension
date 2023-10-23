15b3847fd0ffb46442c52297079a85dea1a71a04
michael <michaelgdimmitt@gmail.com>
2023-10-23 12:17:54 -0400

    show keys
    This commit has been edited 1 times

--- stubbed-react-meetup-subscribe-protorype-app/src/pages/MeetupDetails.jsx
+++ stubbed-react-meetup-subscribe-protorype-app/src/pages/MeetupDetails.jsx
@@ -14,32 +14,32 @@ const MeetupDetails = () => {
   // const {id, venue.name, venue.lat, venue.lon, venue.repinned} = venue;
   // const { created, name, id, join_mode, lat } = group;
   return <div>
-      <br/>{ name}
+      name: {name}
       <br/>description:
       <br/><div style={{border: 'solid black 1px', display: 'inline-block', whiteSpace: 'pre-line', padding: '.5rem', margin: '.5rem'}}>{ description.replace(/<\/?[^>]+(>|$)/g, "")}</div>
-      <br/>{ duration}
-      { created}
-      {/* <br/>{ group.created}
-      <br/>{ group.name}
-      <br/>{ group.id}
-      <br/>{ group.join_mode}
-      <br/>{ group.lat} */}
-      <br/>id: { id}
-      <br/>{ link}
-      <br/>{ local_date}
-      <br/>{ local_time}
-      <br/>{ status}
-      <br/>{ JSON.stringify(time)}
-      <br/>{ updated}
-      <br/>{ utc_offset}
-      {/* <br/>{ venue.id}
-      <br/>{ venue.name}
-      <br/>{ venue.lat}
-      <br/>{ venue.lon}
-      <br/>{ venue.repinned} */}
-      <br/>{ visibility}
-      {/* <br/>{ waitlist_count}
-      <br/>{ yes_rsvp_count} */}
+      <br/>created: { created}
+      <br/>duration: { duration}
+      <br/>group.created: { group.created}
+      <br/>group.name: { group.name}
+      <br/>group.id: { group.id}
+      <br/>group.join_mode: { group.join_mode}
+      <br/>group.lat: { group.lat}
+      <br/>id: id: { id}
+      <br/>link: { link}
+      <br/>local_date: { local_date}
+      <br/>local_time: { local_time}
+      <br/>status: { status}
+      <br/>JSON.stringify(time): { JSON.stringify(time)}
+      <br/>updated: { updated}
+      <br/>utc_offset: { utc_offset}
+      <br/>venue.id: { venue.id}
+      <br/>venue.name: { venue.name}
+      <br/>venue.lat: { venue.lat}
+      <br/>venue.lon: { venue.lon}
+      <br/>venue.repinned: { venue.repinned}
+      <br/>visibility: { visibility}
+      <br/>waitlist_count: { waitlist_count}
+      <br/>yes_rsvp_count: { yes_rsvp_count}
   </div>
 }
 export const Component = MeetupDetails
